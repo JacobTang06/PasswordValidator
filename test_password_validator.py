37b2afb7cef3965e0e38cf2ba2c06b79e133a31b
Jacob Tang <jake.tang@gmail.com>
2025-04-06 22:11:23 -0700

    updated test cases

---
+++ test_password_validator.py
@@ -0,0 +1,30 @@
+
+class TestPasswordValidator:
+
+    def test_password_length(self):
+        assert validate_password("Val123!") == False
+        assert validate_password("ValidatePassword12345!") == False
+
+    def test_password_uppercase_letter(self):
+        assert validate_password("validpass123!") == False
+
+    def test_password_lowercase_letter(self):
+        assert validate_password("VALIDPASS123!") == False
+    
+    def test_password_digit(self):
+        assert validate_password("ValidPass!") == False
+
+    def test_password_special_char(self):
+        assert validate_password("ValidPass123") == False
+
+    def test_password_invalid_char(self):
+        assert validate_password("Invalid<Pass123!") == False
+        assert validate_password("Invalid Pass123!") == False
+        assert validate_password("InvalidPass123!]") == False
+
+    def test_password_repeated_char(self):
+        assert validate_password("InvalidPasss123!") == False
+        assert validate_password("InvalidddddPass123!") == False
+
+    def test_password_success(self):
+        assert validate_password("ValidPass123!") == True
